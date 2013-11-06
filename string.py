2c7373e76881e2194d0d9779623377051073f4f1
Fahim121 <cse.fahimfaysal@yahoo.com>
2013-11-07 04:31:48 +0600

    Create string.py

---
+++ string.py
@@ -0,0 +1,21 @@
+import time;
+print "Enter a string how long or short you want.....>"
+str=raw_input(">")
+
+elements=[]
+Length=len(str)
+if Length < 3:
+   for index in range(len(str)-1, -1, -1):
+     elements.append(str[index])
+  
+   for i in elements:
+      print "Element was: %s" % i
+elif Length>=3 and Length<6:
+   print str  
+elif Length>6:
+  print "Your string is more than 6 characters!!"   
+
+
+print "Thank You!"
+localtime = time.asctime( time.localtime(time.time()) )
+print "Local current time :", localtime
